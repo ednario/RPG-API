b0480b835a69a5074ff8bebb1e9f2ec8c6f1053b
Ednario Andrade <ednariocoder@gmail.com>
2023-12-27 10:52:44 -0300

    feat: ensure attribute updates by id

---
+++ src/modules/attributes/useCases/updateAttributes/UpdateAttributesUseCase.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@nestjs/common';
+
+import { Attributes } from '@attributes/entities/attributes';
+
+import { AttributesRepository } from '@attributes/repositories/AttributesRepository';
+
+interface UpdateAttributesUseCaseRequest {
+  id: string;
+  hp: number;
+  mp: number;
+  strength: number;
+  agility: number;
+  dexterity: number;
+  constitution: number;
+  intelligence: number;
+}
+
+interface UpdateAttributesUseCaseResponse {
+  attributes: Attributes;
+}
+
+@Injectable()
+export class UpdateAttributesUseCase {
+  constructor(private attributesRepository: AttributesRepository) {}
+
+  async execute(
+    request: UpdateAttributesUseCaseRequest,
+  ): Promise<UpdateAttributesUseCaseResponse> {
+    const {
+      id,
+      hp,
+      mp,
+      strength,
+      agility,
+      dexterity,
+      constitution,
+      intelligence,
+    } = request;
+    const attributesExists = await this.attributesRepository.findById(id);
+
+    if (!attributesExists) {
+      throw new Error('Attributes not found');
+    }
+
+    attributesExists.hp = hp;
+    attributesExists.mp = mp;
+    attributesExists.strength = strength;
+    attributesExists.agility = agility;
+    attributesExists.dexterity = dexterity;
+    attributesExists.constitution = constitution;
+    attributesExists.intelligence = intelligence;
+
+    await this.attributesRepository.update(attributesExists);
+
+    return { attributes: attributesExists };
+  }
+}
